38df03118e2573c12f02bc657686d3ce9e3d6cd3
mtungij <jamesmtungi2013@gmail.com>
2025-02-19 07:37:44 +0300

    sms

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 // $config['base_url'] = 'http://localhost:8079/'; 
-$config['base_url'] = 'https://fordmicrofinance.loan-pocket.com/'; 
+$config['base_url'] = 'https://nackcredit.loan-pocket.com/'; 
 /*
 |--------------------------------------------------------------------------
 | Index File
