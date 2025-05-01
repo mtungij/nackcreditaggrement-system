93b988e3883e4dbd964bc11964addad5d50f658d
mtungij <jamesmtungi2013@gmail.com>
2025-05-01 10:12:29 +0300

    sms

--- application/controllers/Oficer.php
+++ application/controllers/Oficer.php
@@ -5580,20 +5580,31 @@ $days_remain = $this->queries->get_loan_active_customer($customer_id);
 // }
 
 public function sendsms($phone,$massage){
-    //$phone = '0753871034';
-    //$sms = 'mapenzi yanauwa';
-    $api_key = '';
-    //$api_key = 'qFzd89PXu1e/DuwbwxOE5uUBn6';
-    //$curl = curl_init();
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,"https://galadove.loan-pocket.com/api/v1/receive/action/send/sms");
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS,
-            'apiKey='.$api_key.'&phoneNumber='.$phone.'&messageContent='.$massage);
+	//public function sendsms(){f
+	//$phone = '255628323760';
+	//$massage = 'mapenzi yanauwa';
+	$api_key = '';                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+	//$api_key = 'qFzd89PXu1e/DuwbwxOE5uUBn6';
+	//$curl = curl_init();
+  $url = "https://sms-api.kadolab.com/api/send-sms";
+  $token = "16|QKnHLafMpXvah7gGXMY3E0jOjs9Urw7vUKmwDvQqe65546a6";
 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Authorization: Bearer '. $token,
+    'Content-Type: application/json',
+  ]);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+    "phoneNumbers" => ["+$phone"],
+    "message" => $massage
+  ]));
+
 $server_output = curl_exec($ch);
 curl_close ($ch);
+
+//print_r($server_output);
 }
 
 
